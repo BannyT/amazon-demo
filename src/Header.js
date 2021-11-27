8995610aa4ea84c94ee3f738e2af5b659c0480b9
BannyT <tumusiimebarnabus@gmail.com>
2021-11-27 13:26:36 +0300

    training

---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import "./Header.css"
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+const Header = () => {
+  return (
+    <div className="main-division">
+        <img className="logo" src="https://www.amazon.com/ref=nav_logo"/>
+         <h5 className="location">Makerere,Kampala</h5>
+         <div className="search-bar">
+             <input type="text" placeholder="Search for products"  />
+         </div>
+         <div className="options">
+              <div className="login-details">
+                  <small>
+                      login
+                  </small>
+                  <small>
+                      SignUp
+                  </small>
+              </div>
+              <div className="Cart">
+                 <ShoppingCartIcon/>
+              </div>
+         </div>
+    </div>
+  )
+}
+
+export default Header
