5a1954768629034eef0622debd26b91254dee177
BannyT <tumusiimebarnabus@gmail.com>
2021-12-03 14:24:03 +0300

    uodates banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Banner  from './Banner';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Banner/>
     </div>
   );
 }
